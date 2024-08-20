0f0667b340b20cb9e527057d36753dc7754940f6
bluesky7660 <chlee7660@gmail.com>
2024-08-20 17:26:50 +0900

    0820 3

--- user1/assets/js/custom.js
+++ user1/assets/js/custom.js
@@ -688,5 +688,36 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // // 페이지 로드 시 초기 계산 수행
     // updateTotalCash();
+    const heartIcons = document.querySelectorAll('.heart_icon');
     
+    heartIcons.forEach(heartIcon => {
+        const img = heartIcon.querySelector('img');
+        if (img) {
+            img.remove();
+        }
+        heartIcon.addEventListener('click', function() {
+            this.classList.toggle('active');
+        });
+    });
+   
+    
+
+    // .review_list 요소와 모든 span 요소를 선택합니다.
+    const reviewListElement = document.querySelector('.review_list>ul');
+    const spans = document.querySelectorAll('.review_count');
+
+    // .review_list의 직접 자식 <li> 요소만을 선택하여 개수를 셉니다.
+    function countDirectChildren(element) {
+        // 자식 요소 중 <li>만 선택
+        const directLiItems = Array.from(element.children).filter(child => child.tagName === 'LI');
+        return directLiItems.length;
+    }
+
+    // .review_list 내부의 직접 자식 <li> 요소의 개수를 계산합니다.
+    const itemCount = countDirectChildren(reviewListElement);
+    
+    // span 요소에 개수를 출력합니다.
+    spans.forEach(span => {
+        span.textContent = itemCount;
+    });
 });
