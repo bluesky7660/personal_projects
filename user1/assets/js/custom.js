9d868517a0f3ac5358b9add8c3510fa9d3e25a7d
bluesky7660 <chlee7660@gmail.com>
2024-08-21 09:38:11 +0900

    0821 1

--- user1/assets/js/custom.js
+++ user1/assets/js/custom.js
@@ -688,6 +688,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // // 페이지 로드 시 초기 계산 수행
     // updateTotalCash();
+
+    //하트
     const heartIcons = document.querySelectorAll('.heart_icon');
     
     heartIcons.forEach(heartIcon => {
@@ -702,22 +704,15 @@ document.addEventListener('DOMContentLoaded', function () {
    
     
 
-    // .review_list 요소와 모든 span 요소를 선택합니다.
-    const reviewListElement = document.querySelector('.review_list>ul');
-    const spans = document.querySelectorAll('.review_count');
-
-    // .review_list의 직접 자식 <li> 요소만을 선택하여 개수를 셉니다.
-    function countDirectChildren(element) {
-        // 자식 요소 중 <li>만 선택
-        const directLiItems = Array.from(element.children).filter(child => child.tagName === 'LI');
-        return directLiItems.length;
-    }
-
-    // .review_list 내부의 직접 자식 <li> 요소의 개수를 계산합니다.
-    const itemCount = countDirectChildren(reviewListElement);
     
-    // span 요소에 개수를 출력합니다.
-    spans.forEach(span => {
-        span.textContent = itemCount;
-    });
+
+    //바로구매링크
+    const directBuyBtn = document.querySelectorAll('.direct_buy_btn');
+
+    directBuyBtn.forEach((item,index)=>{
+        item.addEventListener('click',function(){
+            window.location.href = 'product_buy.html';
+        });
+        
+    });    
 });
